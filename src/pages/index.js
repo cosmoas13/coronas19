581f774db91ce04a2fdaa16a38d8abaf0247b8ae
cosmoas13 <cosmoas13@gmail.com>
2020-04-08 10:13:23 +0700

    without Rain

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,186 +50,186 @@ class Home extends Component {
         <Container>
           <Header />
           <br />
-          <Row>
-            <Col xs={12} md={4}>
-              <Card className="card-style">
-                <Container>
-                  <Form>
+          <div>
+            <Row>
+              <Col xs={12} md={4}>
+                <Card className="card-style">
+                  <Container>
+                    <Form>
+                      <img
+                        alt=""
+                        src="/images/world.png"
+                        width="70"
+                        height="70"
+                        className="d-inline-block align-top"
+                      />
+                      <h3>Country</h3>
+                      <Form.Group controlId="exampleForm.SelectCustom">
+                        <Form.Label style={{ color: "aliceblue" }}>
+                          Select Country
+                        </Form.Label>
+                        <Form.Control
+                          style={{ backgroundColor: "transparent" }}
+                          as="select"
+                          custom
+                          value={this.state.iso3}
+                          onChange={e => {
+                            console.log(e.target.value);
+                            this.getConfirmed(e.target.value);
+                          }}
+                        >
+                          {country != null ? (
+                            country.map((item, index) => (
+                              <option key={index} value={item.iso3}>
+                                {item.name}
+                              </option>
+                            ))
+                          ) : (
+                            <option>Loading</option>
+                          )}
+                        </Form.Control>
+                      </Form.Group>
+                    </Form>
+                  </Container>
+                </Card>
+              </Col>
+              <Col className="col-style" xs={12} md={4}>
+                <Card className="card-style">
+                  <Container>
                     <img
                       alt=""
-                      src="/images/world.png"
+                      src="/images/note.svg"
                       width="70"
                       height="70"
                       className="d-inline-block align-top"
                     />
-                    <h3>Country</h3>
-                    <Form.Group controlId="exampleForm.SelectCustom">
-                      <Form.Label style={{ color: "aliceblue" }}>
-                        Select Country
-                      </Form.Label>
-                      <Form.Control
-                        style={{ backgroundColor: "transparent" }}
-                        as="select"
-                        custom
-                        value={this.state.iso3}
-                        onChange={e => {
-                          console.log(e.target.value);
-                          this.getConfirmed(e.target.value);
-                        }}
-                      >
-                        {country != null ? (
-                          country.map((item, index) => (
-                            <option key={index} value={item.iso3}>
-                              {item.name}
-                            </option>
-                          ))
-                        ) : (
-                          <option>Loading</option>
-                        )}
-                      </Form.Control>
-                    </Form.Group>
-                  </Form>
-                </Container>
-              </Card>
-            </Col>
-            <Col className="col-style" xs={12} md={4}>
-              <Card className="card-style">
-                <Container>
-                  <img
-                    alt=""
-                    src="/images/note.svg"
-                    width="70"
-                    height="70"
-                    className="d-inline-block align-top"
-                  />
-                  <h3>Confirmed</h3>
-                  <h5>{data?.confirmed?.value}</h5>
-                </Container>
-              </Card>
-            </Col>
-            <Col className="col-style" xs={12} md={4}>
-              <Card className="card-style">
-                <Container>
-                  <img
-                    alt=""
-                    src="/images/recover.svg"
-                    width="70"
-                    height="70"
-                    className="d-inline-block align-top"
-                  />
-                  <h3>Recovered</h3>
-                  <h5>{data?.recovered?.value}</h5>
-                </Container>
-              </Card>
-            </Col>
-          </Row>
-          <br />
-          <Row>
-            <Col className="col-style" xs={12} md={8}>
-              <Card className="card-style">
-                <Container>
-                  <img
-                    alt=""
-                    src="/images/death.svg"
-                    width="70"
-                    height="70"
-                    className="d-inline-block align-top"
-                  />
-                  <h3>Death</h3>
-                  <h5>{data?.deaths?.value}</h5>
-                </Container>
-              </Card>
-            </Col>
-            <Col className="col-style" xs={12} md={4}>
-              <Card className="card-style">
-                <Container>
-                  <img
-                    alt=""
-                    src="/images/clock.svg"
-                    width="70"
-                    height="70"
-                    className="d-inline-block align-top"
-                  />
-                  <h3>Last Update</h3>
-                  <h5>
-                    {moment(data?.lastUpdate)
-                      .subtract(3, "days")
-                      .calendar()}
-                  </h5>
-                </Container>
-              </Card>
-            </Col>
-          </Row>
-          <br />
-          <Row>
-            <Col className="col-style" xs={12} md={12}>
-              <Card>
-                <Container
-                  style={{
-                    backgroundColor: "#3a2d38",
-                    borderRadius: "15px",
-                    borderColor: "#3a2d38",
-                    color: "#3a2d38"
-                  }}
-                >
-                  <Chart legendPosition="bottom" />
-                </Container>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={12}>
-              <Container className="parentFoot">
-                <div
-                  style={{ display: "flex", justifyContent: "space-around" }}
-                >
-                  <div className="footer">
-                    <a href="https://github.com/cosmoas13">
-                      <img
-                        alt=""
-                        src="/images/github.svg"
-                        width="25"
-                        height="25"
-                        className="d-inline-block align-top"
-                      />
-                    </a>
-                  </div>
-                  <div className="footer">
-                    <a href="https://www.facebook.com/cosmo.as">
-                      <img
-                        alt=""
-                        src="/images/facebook.svg"
-                        width="25"
-                        height="25"
-                        className="d-inline-block align-top"
-                      />
-                    </a>
-                  </div>
+                    <h3>Confirmed</h3>
+                    <h5>{data?.confirmed?.value}</h5>
+                  </Container>
+                </Card>
+              </Col>
+              <Col className="col-style" xs={12} md={4}>
+                <Card className="card-style">
+                  <Container>
+                    <img
+                      alt=""
+                      src="/images/recover.svg"
+                      width="70"
+                      height="70"
+                      className="d-inline-block align-top"
+                    />
+                    <h3>Recovered</h3>
+                    <h5>{data?.recovered?.value}</h5>
+                  </Container>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="col-style" xs={12} md={8}>
+                <Card className="card-style">
+                  <Container>
+                    <img
+                      alt=""
+                      src="/images/death.svg"
+                      width="70"
+                      height="70"
+                      className="d-inline-block align-top"
+                    />
+                    <h3>Death</h3>
+                    <h5>{data?.deaths?.value}</h5>
+                  </Container>
+                </Card>
+              </Col>
+              <Col className="col-style" xs={12} md={4}>
+                <Card className="card-style">
+                  <Container>
+                    <img
+                      alt=""
+                      src="/images/clock.svg"
+                      width="70"
+                      height="70"
+                      className="d-inline-block align-top"
+                    />
+                    <h3>Last Update</h3>
+                    <h5>
+                      {moment(data?.lastUpdate)
+                        .subtract(3, "days")
+                        .calendar()}
+                    </h5>
+                  </Container>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="col-style" xs={12} md={12}>
+                <Card>
+                  <Container
+                    style={{
+                      backgroundColor: "#3a2d38",
+                      borderRadius: "15px",
+                      borderColor: "#3a2d38",
+                      color: "#3a2d38"
+                    }}
+                  >
+                    <Chart legendPosition="bottom" />
+                  </Container>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={12}>
+                <Container className="parentFoot">
+                  <div
+                    style={{ display: "flex", justifyContent: "space-around" }}
+                  >
+                    <div className="footer">
+                      <a href="https://github.com/cosmoas13">
+                        <img
+                          alt=""
+                          src="/images/github.svg"
+                          width="25"
+                          height="25"
+                          className="d-inline-block align-top"
+                        />
+                      </a>
+                    </div>
+                    <div className="footer">
+                      <a href="https://www.facebook.com/cosmo.as">
+                        <img
+                          alt=""
+                          src="/images/facebook.svg"
+                          width="25"
+                          height="25"
+                          className="d-inline-block align-top"
+                        />
+                      </a>
+                    </div>
 
-                  <div className="footer">
-                    <a href="https://www.instagram.com/cosmo.as/">
-                      <img
-                        alt=""
-                        src="/images/instagram.svg"
-                        width="25"
-                        height="25"
-                        className="d-inline-block align-top"
-                      />
-                    </a>
+                    <div className="footer">
+                      <a href="https://www.instagram.com/cosmo.as/">
+                        <img
+                          alt=""
+                          src="/images/instagram.svg"
+                          width="25"
+                          height="25"
+                          className="d-inline-block align-top"
+                        />
+                      </a>
+                    </div>
                   </div>
-                </div>
-              </Container>
-            </Col>
-          </Row>
-          <br />
-          <Row>
-            <Col xs={12} md={12}>
-              <p style={{ color: "gray" }}>
-                created by &copy;cosmoas13 - API from{" "}
-                <a href="https://covid19.mathdro.id/api/">@mathdro.id</a>
-              </p>
-            </Col>
-          </Row>
+                </Container>
+              </Col>
+            </Row>
+            <br />
+            <Row>
+              <Col xs={12} md={12}>
+                <p style={{ color: "gray" }}>
+                  created by &copy;cosmoas13 - API from{" "}
+                  <a href="https://covid19.mathdro.id/api/">@mathdro.id</a>
+                </p>
+              </Col>
+            </Row>
+          </div>
         </Container>
       </>
     );
